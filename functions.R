677d0001585e1b97f0d8a4dc9ea39ac92db6178c
wlandau <will.landau@gmail.com>
2019-01-18 01:53:14 -0500

    Improvements

--- functions.R
+++ functions.R
@@ -1,11 +1,12 @@
 # Center and scale the dataset.
-prepare_dataset <- function(dataset) {
-  dataset %>%
+prepare_gapminder <- function(data) {
+  data %>%
     # Log-transform some heavily right-skewed variables.
     mutate(
-      log_gdpPercap = log(gdpPercap), 
+      log_gdp_percap = log(gdpPercap), 
       log_pop = log(pop)
     ) %>%
+    rename(life_exp = lifeExp) %>%
     # Exclude some variables
     select(-gdpPercap, -pop, -country, -continent, -year) %>%
     as.matrix() %>%
@@ -20,7 +21,7 @@ prepare_dataset <- function(dataset) {
 # this function takes a long time if `iter` is high.
 fit_model <- function(covariate, dataset) {
   stan_glm(
-    formula = as.formula(paste("log_gdpPercap ~", covariate)),
+    formula = as.formula(paste("log_gdp_percap ~", covariate)),
     data = dataset,
     family = gaussian(link = "identity"),
     chains = 4,
@@ -37,7 +38,7 @@ plot_model <- function(fit) {
   samples <- as.data.frame(fit)
   colnames(samples)[1] <- "intercept"
   ggplot() +
-    geom_point(aes_string(x = covariate, y = "log_gdpPercap"), fit$data) +
+    geom_point(aes_string(x = covariate, y = "log_gdp_percap"), fit$data) +
     geom_abline(
       aes_string(slope = covariate, intercept = "intercept"),
       samples,
